5b13dda32b7b85aaecc6dce445334dd5ca7b9cf2
N. Engelhardt <nak@symbioticeda.com>
2020-10-19 16:07:07 +0200

    add memory range checks and a memory-mapped output register

--- stupidrv_tb.sv
+++ stupidrv_tb.sv
@@ -23,15 +23,40 @@ always @(posedge clock) reset <= 0;
 
 reg [31:0] mem [0:(1<<MEM_ADDR_WIDTH)-1];
 
+wire wr_in_mem_range = (dmem_addr[31:2] < (1<<MEM_ADDR_WIDTH));
+wire wr_in_output = (dmem_addr == 32'h 02000000);
+
+reg [31:0] out;
+reg out_valid;
+always @(posedge clock) begin
+	if (out_valid) $display("Output: %d", out);
+end
+
+always @(posedge clock) begin
+	if (imem_addr[31:2] >= (1<<MEM_ADDR_WIDTH)) begin
+		$display("Memory access out of range: imem_addr = 0x%08x", imem_addr);
+	end
+	if (dmem_valid && !(wr_in_mem_range || wr_in_output)) begin
+		$display("Memory access out of range: dmem_addr = 0x%08x", dmem_addr);
+	end
+end
+
 integer i;
 always @(posedge clock) begin
+	out_valid <= 1'b0;
 	imem_data <= mem[imem_addr[MEM_ADDR_WIDTH+1:2]];
 
 	if (dmem_valid) begin
 		dmem_rdata <= mem[dmem_addr[MEM_ADDR_WIDTH+1:2]];
 		for (i=0;i<4;i=i+1) begin
 			if (dmem_wstrb[i]) begin
-				mem[dmem_addr[MEM_ADDR_WIDTH+1:2]][(i*8)+: 8] <= dmem_wdata[(i*8)+: 8];
+				if (wr_in_mem_range) begin
+					mem[dmem_addr[MEM_ADDR_WIDTH+1:2]][(i*8)+: 8] <= dmem_wdata[(i*8)+: 8];
+				end
+				if (wr_in_output) begin
+					out[(i*8)+: 8] <= dmem_wdata[(i*8)+: 8];
+					out_valid <= 1'b1;
+				end
 			end
 		end
 	end else begin
@@ -66,7 +91,7 @@ reg [31:0] cycles = 0;
 
 always @(posedge clock) begin
 	cycles <= cycles + 32'h1;
-	if (cycles > TIMEOUT) begin
+	if (cycles >= TIMEOUT) begin
 		$display("Simulated %d cycles", cycles);
 		$finish;
 	end
