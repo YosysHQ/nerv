88fe34d2a1533866b26d9b4ae06b75087f1c5112
Miodrag Milanovic <mmicko@gmail.com>
2020-10-20 14:18:11 +0200

    Fixed SP and add new lds and soubroutine test

--- firmware.c
+++ firmware.c
@@ -1,7 +1,36 @@
-volatile int * const outreg = (int*)0x02000000;
+/*
+ *  PicoRV -- A Small and Extensible RISC-V Processor
+ *
+ *  Copyright (C) 2019  Claire Wolf <claire@symbioticeda.com>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+void putc(int c)
+{
+	volatile char *p = (void*)0x02000000;
+	*p = c;
+}
+
+void puts(char *s)
+{
+	while (*s) putc(*(s++));
+}
 
 int main()
 {
-	*outreg = 42;
+	puts("Hello World!\n");
+	putc(0);
 	return 0;
 }
