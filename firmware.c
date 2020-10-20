e961420c20ee20f369ff207aea07b8c2a70bb604
Claire Xenia Wolf <claire@symbioticeda.com>
2020-10-20 22:04:55 +0200

    Add copyright
    Signed-off-by: Claire Xenia Wolf <claire@symbioticeda.com>

--- firmware.c
+++ firmware.c
@@ -1,7 +1,7 @@
 /*
- *  PicoRV -- A Small and Extensible RISC-V Processor
+ *  NERV -- Naive Educational RISC-V Processor
  *
- *  Copyright (C) 2019  Claire Wolf <claire@symbioticeda.com>
+ *  Copyright (C) 2020  Claire Xenia Wolf <claire@symbioticeda.com>
  *
  *  Permission to use, copy, modify, and/or distribute this software for any
  *  purpose with or without fee is hereby granted, provided that the above
