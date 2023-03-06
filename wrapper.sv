a8637d1804b88228651d2d9af4d30d9a2a667ecd
Jannis Harder <me@jix.one>
2023-03-06 17:12:59 +0100

    Add new formal memory bus interface to formal wrapper

--- wrapper.sv
+++ wrapper.sv
@@ -21,6 +21,7 @@ module rvfi_wrapper (
 	input         clock,
 	input         reset,
 	`RVFI_OUTPUTS
+	`RVFI_BUS_OUTPUTS
 );
 	(* keep *) `rvformal_rand_reg stall;
 	(* keep *) `rvformal_rand_reg [31:0] imem_data;
@@ -54,6 +55,67 @@ module rvfi_wrapper (
 
 		`RVFI_CONN32
 	);
+
+`ifdef RISCV_FORMAL_BUS
+
+`define RISCV_FORMAL_CHANNEL_SIGNAL(channels, width, name) \
+	(* keep *) reg [(width) - 1:0] imem_``name; assign rvfi_``name[0 * (width) +: (width)] = imem_``name;
+`RVFI_BUS_SIGNALS
+`undef RISCV_FORMAL_CHANNEL_SIGNAL
+
+`define RISCV_FORMAL_CHANNEL_SIGNAL(channels, width, name) \
+	(* keep *) reg [(width) - 1:0] dmem_``name; assign rvfi_``name[1 * (width) +: (width)] = dmem_``name;
+`RVFI_BUS_SIGNALS
+`undef RISCV_FORMAL_CHANNEL_SIGNAL
+
+
+	reg [31:0] imem_addr_q;
+
+	always @(posedge clock) begin
+		if (!stall)
+			imem_addr_q <= imem_addr;
+	end
+
+	always @* begin
+		imem_bus_addr  = imem_addr_q;
+		imem_bus_insn  = 1;
+		imem_bus_data  = 0;
+		imem_bus_rmask = 4'b1111;
+		imem_bus_wmask = 4'b0000;
+		imem_bus_rdata = imem_data;
+		imem_bus_wdata = 0;
+		imem_bus_valid = !stall;
+	end;
+
+	reg        dmem_valid_q;
+	reg [31:0] dmem_addr_q;
+	reg [ 3:0] dmem_wstrb_q;
+	reg [31:0] dmem_wdata_q;
+
+
+	(* keep *) `rvformal_rand_reg [31:0] next_dmem_rdata;
+	reg [31:0] next_dmem_rdata_q;
+
+	always @(posedge clock) begin
+		if (!stall)
+			next_dmem_rdata_q <= next_dmem_rdata;
+	end
+
+	always @* begin
+		if (!stall)
+			assume (dmem_rdata == next_dmem_rdata_q);
+
+		dmem_bus_addr  = dmem_addr;
+		dmem_bus_insn  = 0;
+		dmem_bus_data  = 1;
+		dmem_bus_rmask = dmem_wstrb ? 4'b0000 : 4'b1111;
+		dmem_bus_wmask = dmem_wstrb;
+		dmem_bus_rdata = next_dmem_rdata;
+		dmem_bus_wdata = dmem_wdata;
+		dmem_bus_valid = !stall && dmem_valid;
+	end;
+
+`endif
 
 `ifdef NERV_FAIRNESS
 	reg [2:0] stalled = 0;
